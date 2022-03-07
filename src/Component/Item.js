df9fcaf619acb1a4ded81143e2af8769750aa1ac
Chaiyut <benzchaiyut.k@gmail.com>
2022-03-08 00:22:38 +0700

    use array map to map Item Component

--- src/Component/Item.js
+++ src/Component/Item.js
@@ -1,7 +1,9 @@
 function Item(props)
 {
+    const {data} = props;
+    
     return(
-        <li className="item">{props.title} <span>-{props.amount} บาท</span></li>
+        <li className="item">{data.title} <span>{data.amount} บาท</span></li>
     );
 }
 export default Item;
--- src/Component/Transaction.js
+++ src/Component/Transaction.js
@@ -3,10 +3,17 @@ import "./Transaction.css";
 
 function Transaction()
 {
+    const data = [
+        {title:"ค่ารักษาพยาบาล",amount:"-2000"},
+        {title:"เงินเดือน",amount:"20000"}
+];
+const itemsElement = data.map((e,i)=>{
+    return <Item data={e} key={i} />
+})
     return(
     <div className="frame-list">
     <ul className="item-list">  
-        <Item title="ค่ารักษาพยาบาล" amount="2000"/>
+    {itemsElement}
     </ul>
     </div>
     );
